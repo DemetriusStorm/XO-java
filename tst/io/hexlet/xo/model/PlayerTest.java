991f1be363f0b0df84ffd7d2c11bec10725dc52e
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-01-28 16:25:06 +0300

    Player tests change variable name inline

--- tst/io/hexlet/xo/model/PlayerTest.java
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -8,25 +8,23 @@ class PlayerTest {
 
     @Test
     void getName() {
-        final String inputValue = "Dima";
-        final String expectedValue = inputValue;
+        final String expectedInputValue = "Dima";
 
-        final Player player = new Player(inputValue, null);
+        final Player player = new Player(expectedInputValue, null);
 
         final String actualValue = player.getName();
 
-        assertEquals(expectedValue, actualValue);
+        assertEquals(expectedInputValue, actualValue);
     }
 
     @Test
     void getFigure() {
-        final Figure inputValue = Figure.X;
-        final Figure expectedValue = inputValue;
+        final Figure expectedInputValue = Figure.X;
 
-        final Player player = new Player(null, inputValue);
+        final Player player = new Player(null, expectedInputValue);
 
         final Figure actualValue = player.getFigure();
 
-        assertEquals(expectedValue, actualValue);
+        assertEquals(expectedInputValue, actualValue);
     }
 }
