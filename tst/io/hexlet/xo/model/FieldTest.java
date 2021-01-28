00d0610349cf70549ccf7e07a2db8f00e1aafa54
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-01-28 22:56:07 +0300

    Field test have been fixed

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -31,20 +31,6 @@ class FieldTest {
     }
 
     @Test
-    void setFigureWhenAlreadyOccupied() throws Exception {
-        final Field field = new Field();
-        final Point inputPoint = new Point(0, 0);
-        final Figure inputFigure = Figure.O;
-
-        field.setFigure(inputPoint, inputFigure);
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail();
-        } catch (final AlreadyOccupiedException ignored) {}
-
-    }
-
-    @Test
     void getFigureWhenFigureIsNotSet() throws Exception {
         final Field field = new Field();
         final Point inputPoint = new Point(0, 0);
