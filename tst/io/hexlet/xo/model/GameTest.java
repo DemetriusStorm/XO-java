8f0ab62456e5839510fcda902c61255ac4a42c60
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-01-28 22:40:37 +0300

    Game test added GameName exception

--- tst/io/hexlet/xo/model/GameTest.java
+++ tst/io/hexlet/xo/model/GameTest.java
@@ -1,9 +1,10 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.InvalidGameNameException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,13 +27,15 @@ class GameTest {
     }
 
     @Test
-    void getName() {
-        final String expectedValue = "Dima";
+    void getName() throws Exception {
+        final String expectedValue = "XO";
+        final String expectedNullValue = null;
 
         final Game game = new Game(null, null, expectedValue);
 
         final String actualValue = game.getName();
 
         assertEquals(expectedValue, actualValue);
+        assertNull(expectedNullValue);
     }
 }
