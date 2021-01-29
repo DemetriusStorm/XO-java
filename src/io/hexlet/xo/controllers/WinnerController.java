fe21782618873fd274b2f79e0ffd408fe31d6cad
Dmitriy Bulygin <demetrius.storm@gmail.com>
2021-01-29 12:38:42 +0300

    Fixed for WinnerController (where fixes the issues, that were discovered during unit testing)

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -15,7 +15,7 @@ public class WinnerController {
                     return field.getFigure(new Point(i, 0));
 
             for (int i = 0; i < 3; i++)
-                if (check(field, new Point(i, 0), p -> new Point(p.x + 1, p.y)))
+                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
                     return field.getFigure(new Point(0, i));
 
             if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
@@ -24,25 +24,29 @@ public class WinnerController {
             if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
                 return field.getFigure(new Point(1, 1));
 
-        } catch (InvalidPointException e) {
+        } catch (final InvalidPointException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator) {
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) {
         final Figure currentFigure;
         final Figure nextFigure;
         final Point nextPoint = pointGenerator.next(currentPoint);
 
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+
+            if (currentFigure == null)
+                return false;
+
+            nextFigure = field.getFigure(nextPoint);
         } catch (final InvalidPointException e) {
             return true;
         }
-
-        if (currentFigure == null) return false;
 
         if (currentFigure != nextFigure) return false;
 
